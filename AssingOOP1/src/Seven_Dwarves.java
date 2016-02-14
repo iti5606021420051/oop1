71ebd55622d910b7c469f34b2e8bc3aa04ab4e3e
iti5606021420051 <5606021420051@fitm.kmutnb.ac.th>
2016-02-14 15:47:14 +0700

    Update Seven_Dwarves.java

--- AssingOOP1/src/Seven_Dwarves.java
+++ AssingOOP1/src/Seven_Dwarves.java
@@ -29,7 +29,6 @@ public class Seven_Dwarves
 			num3 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People3 : ","Input",JOptionPane.QUESTION_MESSAGE));
 			System.out.println();
 		}
-		System.out.printf("Enter People4 : ");
 		num4 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People4 : ","Input",JOptionPane.QUESTION_MESSAGE));
 		while(num4 >= 100 || num4 == num3 || num4 == num2 || num4 == num1)
 		{
@@ -37,40 +36,35 @@ public class Seven_Dwarves
 			num4 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People4 : ","Input",JOptionPane.QUESTION_MESSAGE));
 			System.out.println();
 		}
-		System.out.printf("Enter People5 : ");
-		num5 = gold.nextInt();
+		num5 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People4 : ","Input",JOptionPane.QUESTION_MESSAGE));
 		while(num5 >= 100 || num5 == num4 || num5 == num3 || num5 == num2 || num5 == num1)
 		{
 			JOptionPane.showMessageDialog(null,"Error Score","Message",JOptionPane.ERROR_MESSAGE);
 			num5 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People5 : ","Input",JOptionPane.QUESTION_MESSAGE));
 			System.out.println();
 		}
-		System.out.printf("Enter People6 : ");
-		num6 = gold.nextInt();
+		num6 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People6 : ","Input",JOptionPane.QUESTION_MESSAGE));
 		while(num6 >= 100 || num6 == num5 || num6 == num4 || num6 == num3 || num6 == num2 || num6 == num1)
 		{
 			JOptionPane.showMessageDialog(null,"Error Score","Message",JOptionPane.ERROR_MESSAGE);
 			num6 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People6 : ","Input",JOptionPane.QUESTION_MESSAGE));
 			System.out.println();
 		}
-		System.out.printf("Enter People7 : ");
-		num7 = gold.nextInt();
+		num7 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People7 : ","Input",JOptionPane.QUESTION_MESSAGE));
 		while(num7 >= 100 || num7 == num6 || num7 == num5 || num7 == num4 || num7 == num3 || num7 == num2 || num7 == num1)
 		{
 			JOptionPane.showMessageDialog(null,"Error Score","Message",JOptionPane.ERROR_MESSAGE);
 			num7 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People7 : ","Input",JOptionPane.QUESTION_MESSAGE));
 			System.out.println();
 		}
-		System.out.printf("Enter People8 : ");
-		num8 = gold.nextInt();
+		num8 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People8 : ","Input",JOptionPane.QUESTION_MESSAGE));
 		while(num8 >= 100 || num8 == num7 || num8 == num6 || num8 == num5 || num8 == num4 || num8 == num3 || num8 == num2 || num8 == num1)
 		{
 			JOptionPane.showMessageDialog(null,"Error Score","Message",JOptionPane.ERROR_MESSAGE);
 			num8 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People8 : ","Input",JOptionPane.QUESTION_MESSAGE));
 			System.out.println();
 		}
-		System.out.printf("Enter People9 : ");
-		num9 = gold.nextInt();
+		num9 = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter People9 : ","Input",JOptionPane.QUESTION_MESSAGE));
 		while(num9 >= 100 || num9 == num8 || num9 == num7 || num9 == num6 || num9 == num5 || num9 == num4 || num9 == num3 || num9 == num2 || num9 == num1)
 		{
 			JOptionPane.showMessageDialog(null,"Error Score","Message",JOptionPane.ERROR_MESSAGE);
